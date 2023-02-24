5c943172f7678564b6eb9ab48aa219c003dabeb7
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2023-02-24 23:57:17 +0200

    add host

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Iryna:5O9Mj36BeGTYNrfv@cluster0.1420r1l.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
 
