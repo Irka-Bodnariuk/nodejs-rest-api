b19b919d0f73098853c5ff0688659fad61fdbd09
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2023-02-26 22:41:17 +0200

    1. Створила новий проект і налаштувала його на MongoDB. 2. Налаштувала підключення своєї хмарної бази даних до Compass 3. Створила базу даних і в ній колекцію 4. Написала код для створення підключення до MongoDB та додала оновлення статусу favorite

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,14 +1,15 @@
-const contacts = require("../models/contacts");
+const { Contact } = require("../models/contact");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
-  const allContacts = await contacts.listContacts();
+  const allContacts = await Contact.find();
   res.json(allContacts);
 };
+
 const getById = async (req, res) => {
   const { contactId } = req.params;
-  const oneContact = await contacts.getContactById(contactId);
+  const oneContact = await Contact.findById(contactId);
 
   if (!oneContact) {
     throw HttpError(404, "Not found");
@@ -16,14 +17,15 @@ const getById = async (req, res) => {
 
   res.json(oneContact);
 };
+
 const add = async (req, res) => {
-  const newContact = await contacts.addContact(req.body);
+  const newContact = await Contact.create(req.body);
   res.status(201).json(newContact);
 };
 
 const deleteByID = async (req, res) => {
   const { contactId } = req.params;
-  const removeContact = await contacts.removeContact(contactId);
+  const removeContact = await Contact.findByIdAndRemove(contactId);
   if (!removeContact) {
     throw HttpError(404, "Not found");
   }
@@ -33,13 +35,33 @@ const deleteByID = async (req, res) => {
 const updateByID = async (req, res) => {
   const { contactId } = req.params;
 
-  const editContact = await contacts.updateContact(contactId, req.body);
+  const editContact = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!editContact) {
+    throw HttpError(404, "Not found");
+  }
   res.status(200).json(editContact);
 };
+
+const updateStatusContact = async (req, res) => {
+  const { contactId } = req.params;
+
+  const updateStatus = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+
+  if (!updateStatus) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(updateStatus);
+};
+
 module.exports = {
   getAll: ctrlWrapper(getAll),
   getById: ctrlWrapper(getById),
   add: ctrlWrapper(add),
   deleteByID: ctrlWrapper(deleteByID),
   updateByID: ctrlWrapper(updateByID),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,32 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
 
-const { validateBody } = require("../../middlewares");
+const { validateBody, isValidId } = require("../../middlewares");
 
-const schemas = require("../../schemas/contacts");
+const { schemas } = require("../../models/contact");
 
 const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
-router.get("/:contactId", ctrl.getById);
+router.get("/:contactId", isValidId, ctrl.getById);
 
 router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-router.delete("/:contactId", ctrl.deleteByID);
+router.delete("/:contactId", isValidId, ctrl.deleteByID);
 
-router.put("/:contactId", validateBody(schemas.addSchema), ctrl.updateByID);
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(schemas.addSchema),
+  ctrl.updateByID
+);
+
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateStatusContact
+);
 
 module.exports = router;
