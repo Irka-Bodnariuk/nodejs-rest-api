3ff7e8dab62010dd6ee3f281656f6515c094d6a7
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2023-03-08 15:35:26 +0200

    Додала перевірку про відсутність токена

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -9,7 +9,7 @@ const { SECRET_KEY } = process.env;
 const authenticate = async (req, rqs, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
+  if (bearer !== "Bearer" || token === "") {
     next(HttpError(401, "Not authorized"));
   }
   try {
