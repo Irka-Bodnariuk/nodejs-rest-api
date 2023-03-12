0ab7d6688d18bd6103d6157927d7b74b294b8a14
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2023-03-12 20:24:51 +0200

    1. Створила папку public для роздачі статики.

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
+//
+// const multer = require("multer");
+// const path = require("path");
+// const fs = require("fs/promises");
+//
 
 const { contactsRouter, authRouter } = require("./routes");
 
@@ -9,9 +14,22 @@ const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
+//
+// const tempDir = path.join(__dirname, "temp");
+// const multerConfig = multer.diskStorage({
+//   destination: tempDir,
+// });
+// const upload = multer({
+//   storage: multerConfig,
+// });
+// const contacts = [];
+
+//
+
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public")); // 1
 
 app.use("/api/users", authRouter);
 app.use("/api/contacts", contactsRouter);
