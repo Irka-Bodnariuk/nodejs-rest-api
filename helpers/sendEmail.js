e75822e5d0b59465029dae95ba2f6528d76fb9a9
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2023-03-17 21:49:50 +0200

    deleted comments

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -11,25 +11,3 @@ const sendEmail = async (data) => {
   return true;
 };
 module.exports = sendEmail;
-
-// const email = {
-//   to: "xemig81656@loongwin.com",
-//   from: "iryna_bodnariuk@meta.ua",
-//   sudject: "Test email",
-//   html: "<p>TEST email</p>",
-// };
-
-// sgMail
-//   .send(email)
-//   .then(() => console.log("Email sent"))
-//   .catch((error) => console.log(error.mesagge));
-
-//
-
-// const msg = {
-//   to: "test@example.com", // Change to your recipient
-//   from: "test@example.com", // Change to your verified sender
-//   subject: "Sending with SendGrid is Fun",
-//   text: "and easy to do anywhere, even with Node.js",
-//   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-// };
